fb6f6a5a4d2ed3a74ca21ed1e089211e1fe725f6
James Lam <lamjs@mail.uc.edu>
2023-09-17 11:48:27 -0400

    lab3

---
+++ powershell/lab3.ps1
@@ -0,0 +1,18 @@
+﻿$CurrentDate = Get-Date -Format "dddd, MMMM d yyyy"
+$PSVERSION = $PSVersionTable.PSVersion
+$USER = $env:USERNAME
+$HOSTNAME = $env:COMPUTERNAME
+$IPADDRESS = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' }).IPAddress
+$Recipients = @("lamjs@mail.uc.edu", "leonardf@ucmail.uc.edu")
+
+
+$BODY = "This machine's IP is $IPADDRESS. User is $USER. Hostname is $HOSTNAME. PowerShell Version $PSVERSION. Today's Date is $CURRENTDATE"
+
+Write-Output "Recipients = "
+Write-Output $Recipients
+Write-Output "`n"
+
+#
+#Send-MailMessage -To "$Recipients" -From "jlam8931@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
+
+$BODY | Set-content -Path "C:it3038c-scripts\powershell\bodyOuput.txt"
