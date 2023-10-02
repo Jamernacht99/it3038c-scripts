eb44a08bfc791e27ee0b50cf8d2c9ccf11b9e1c9
James Lam <lamjs@mail.uc.edu>
2023-10-02 14:29:55 -0400

    Lab5

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 
-	print("Hello. What is your birthday(MM-DD-YYYY)?")
-	myBirthday = input()
+print("Hello. What is your birthday(MM-DD-YYYY)?")
+myBirthday = input()
 
 try:
 	birthday = datetime.strptime(myBirthday, "%m-%d-%Y")
-execept: ValueError:
+except ValueError:
 	print("Please input a birthday in the valid format 'MM-DD-YYYY'.")
 else:
 	current_date = datetime.now()
-	age_in_seconds = (current_date - birthday).total_seconds()
+	age_in_seconds = int((current_date - birthday).total_seconds())
 
-	print("You are approximately {age_in_seconds} seconds old.")
+	print("You are approximately %s seconds old." % age_in_seconds)
